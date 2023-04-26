c0d380abf0d87ebc99aa9493a3dd8b02964dcfe9
Yushi_61 <108335997+Yushi5058@users.noreply.github.com>
2023-04-26 14:42:53 +0100

    initial commit

---
+++ src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+      home
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+    setup() {
+        
+    },
+    created() {
+
+    },
+    mounted() {
+        
+    }
+}
+</script>
