0edda0e63090da704e4d5c70f1959bf85351be98
Yushi_61 <108335997+Yushi5058@users.noreply.github.com>
2023-04-29 21:51:10 +0100

    fetch data

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,13 @@
 <template>
   <div class="home">
       <h1>home</h1>
-      <PostList v-if="showPosts"  :posts="posts" />
-      <button @click="showPosts = !showPosts">toggle posts</button>
-      <button @click="posts.pop()">delete a post</button>
+      <div v-if="posts.length">
+          <PostList v-if="showPosts"  :posts="posts" />
+          <button @click="showPosts = !showPosts">toggle posts</button>
+          <button @click="posts.pop()">delete a post</button>
+      </div>
+      <div v-if="error">{{ error  }}</div>
+      <div v-else>Loading ...</div>
     </div>
 </template>
 
@@ -15,13 +19,25 @@ export default {
   name: 'Home',
     components: { PostList },
     setup() {
-        const posts = ref([
-            { title: 'welcome to the blog', body: 'Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor Lorem duis laboris cupidatat officia voluptate. Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis.', id: 1 },
-            { title: 'top 5 CSS tips', body: 'Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor Lorem duis laboris cupidatat officia voluptate. Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis.', id: 2 }
-        ])
+        const posts = ref([  ])
+        const error = null
+
+        const load = async() => {
+            try {
+                let data = await fetch('http://localhost:3000/posts')
+                if (!data.ok) {
+                    throw Error('no data available')
+                }
+                posts.value = await data.json()
+            }
+            catch(err) {
+                error.value = err.message
+            }
+        }
+        load()
         const showPosts = ref(true)
 
-        return { posts, showPosts }
+        return { posts, showPosts, error }
     }
 }
 </script>
