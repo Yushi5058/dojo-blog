2f8ccccbc12427053143fa8931cac7bbd37ccb8d
Yushi_61 <108335997+Yushi5058@users.noreply.github.com>
2023-04-29 20:21:48 +0100

    add component PostList

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,24 @@
-template>
+<template>
   <div class="home">
-      home
+      <h1>home</h1>
+      <PostList  :posts="posts" />
     </div>
 </template>
 
 <script>
+import PostList from '../components/PostList.vue';
+import { ref } from 'vue';
 export default {
-  name: 'Home',
-    setup() {
-        
-    },
-    created() {
 
-    },
-    mounted() {
-        
+  name: 'Home',
+    components: { PostList },
+    setup() {
+        const posts = ref([
+            { title: 'welcome to the blog', body: 'lorem ipsum', id: 1 },
+            { title: 'top 5 CSS tips', body: 'lorem ipsum', id: 2 }
+        ])
+
+        return { posts }
     }
 }
 </script>
